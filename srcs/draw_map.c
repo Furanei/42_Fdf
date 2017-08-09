1779347b33ab5116a4ad4564db609276330df7ee
Mikael BRIFFAUD <mbriffau@student.42.fr>
2017-08-10 01:40:57 +0200

    modif line

--- srcs/draw_map.c
+++ srcs/draw_map.c
@@ -6,7 +6,7 @@
 /*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/06/01 04:45:57 by mbriffau          #+#    #+#             */
-/*   Updated: 2017/08/09 19:36:28 by mbriffau         ###   ########.fr       */
+/*   Updated: 2017/08/10 01:37:40 by mbriffau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int pixel_calc(t_3d *d, char option, int right, int down)
 		return((d->start_x + (((d->x + right) + (d->y + down)) * VALUE_VAR_X)));
 	else if (option == 121)
 		return (d->start_y + (((d->y + down) - (d->x + right)) * VALUE_VAR_Y)
-			- (d->map[d->y][(d->x + right)] * (VALUE_VAR_Y)));
+			- (d->map[d->y][(d->x + right)] * 8));
 	else
 	{
 		write(1, "error_pixel_calc\n", ft_strlen("error_pixel_calc\n"));
--- srcs/line.c
+++ srcs/line.c
@@ -6,7 +6,7 @@
 /*   By: mbriffau <mbriffau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/08/08 16:31:12 by mbriffau          #+#    #+#             */
-/*   Updated: 2017/08/09 19:40:45 by mbriffau         ###   ########.fr       */
+/*   Updated: 2017/08/10 01:37:53 by mbriffau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ void	draw_line(t_3d *d)
 	int		x[3];
 	int		y[3];
 	int     range[5];
+	int		i = 0;
+	int pomme = 0;
 
 	ft_bzero(range, 5);
 	x[0] = pixel_calc(d, 'x', 0, 0);
@@ -40,50 +42,41 @@ void	draw_line(t_3d *d)
 	}
 	if (range[4] & 1)
 	{
-		while ((range[0] && range[1]))
-		{	
-			// if (x[0] == x[1] || y[0] == y[0])
-			// 	break;
+		i = ft_abs(range[0]) > ft_abs(range[1]) ? (ft_abs(range[0]) / ft_abs(range[1])) : (ft_abs(range[1]) / ft_abs(range[0]));
+		while (ft_abs(range[0]) || ft_abs(range[1]))
+		{
 			mlx_pixel_put(d->mlx, d->win, x[1],
 			y[1], (d->map[d->y][d->x]) > 2 ? WHITE : RED);
-			if (ft_abs(range[0]) > ft_abs(range[1]))
+			if (ft_abs(range[0]) > ft_abs(range[1]) && pomme <= i)
 			{
-				if (range[0] >= 0)
-				{
-					range[0]--;
-					x[1]--;
-				}
-				else
-				{
-					range[0]++;
-					x[1]++;
-				}
+				range[0] >= 0 ? (range[0]-- && x[1]--) : (range[0]++ && x[1]++);
+				pomme++;
 			}
 			else
 			{
-				if (range[1] >= 0)
-				{
-					range[1]--;
-					y[1]--;
-				}
-				else
-				{
-					range[1]++;
-					y[1]++;
-				}
+				range[1] >= 0 ? (range[1]-- && y[1]--) : (range[1]++ &&	y[1]++ );
+				pomme = 0;
 			}
 		}
 	}
+	pomme = 0;
 	if (range[4] & 2)
 	{
-		while ((range[2] && range[3]))
+		i = ft_abs(range[2]) > ft_abs(range[3]) ? (ft_abs(range[2]) / ft_abs(range[3])) : (ft_abs(range[3]) / ft_abs(range[2]));
+		while (ft_abs(range[2]) || ft_abs(range[3]))
 		{
 			mlx_pixel_put(d->mlx, d->win, x[2],
 			y[2], (d->map[d->y][d->x]) > 2 ? BLUE : GREEN);
-		if (ft_abs(range[2]) > ft_abs(range[3]))
-			range[2] ? (range[2]-- && x[2]--) : (range[2]++ && x[2]++);
-		else
-			range[3] ? (range[3]-- && y[2]--) : (range[3]++ && y[2]++);	
-		}
+			if (ft_abs(range[2]) > ft_abs(range[3]) && pomme <= i)
+			{
+				range[2] > 0 ? (range[2]-- && x[2]--) : (range[2]++ && x[2]++);
+				pomme++;
+			}
+			else
+			{
+				range[3] > 0 ? (range[3]-- && y[2]--) : (range[3]++ && y[2]++);
+				pomme = 0;
+			}
+		}	
 	}
 }
